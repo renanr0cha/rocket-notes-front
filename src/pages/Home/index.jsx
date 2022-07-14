4bff8b797ba2c71bd37060e326b4db85f9376e71
Renan <renan.aragorn@gmail.com>
2022-07-14 01:06:59 -0300

    Note Component and Home page done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,8 @@ import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
 import { Header } from "../../components/Header" 
 import { ButtonText } from "../../components/ButtonText"
 import { Input } from "../../components/Input"
+import { Note } from "../../components/Note"
+import { Section } from "../../components/Section"
 
 export function Home() {
   return (
@@ -28,7 +30,15 @@ export function Home() {
       </Search>
 
       <Content>
-
+        <Section title="Minhas notas">
+          <Note data={{
+            title: "React",
+            tags: [
+              {id: "1", name: "react"},
+              {id: "2", name: "rocketseat"}
+            ]
+          }} />
+        </Section>
       </Content>
 
       <NewNote>
